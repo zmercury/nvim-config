977335d01154c77b2a9e7484a32c6b6eedea892f
zmercury <messiniki@outlook.com>
2025-02-12 16:27:26 +0545

    Update

--- lua/mercury/plugins/tokyonight.lua
+++ lua/mercury/plugins/tokyonight.lua
@@ -3,15 +3,15 @@ return {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require("tokyonight").setup({
-        style = "night",
+      require('tokyonight').setup {
+        style = 'night',
         transparent = true,
         styles = {
-          sidebars = "transparent",
-          floats = "transparent",
+          sidebars = 'transparent',
+          floats = 'transparent',
         },
-      })
-      
+      }
+
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'tokyonight-night'
 
