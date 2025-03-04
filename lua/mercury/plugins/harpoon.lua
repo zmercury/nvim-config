ccce450f50b1a5ecae51db8674b3d04f2601b1cd
zmercury <messiniki@outlook.com>
2025-03-04 12:24:45 +0545

    feat: harpoon navigation added

--- lua/mercury/plugins/harpoon.lua
+++ lua/mercury/plugins/harpoon.lua
@@ -5,37 +5,31 @@ return {
   config = function()
     local harpoon = require 'harpoon'
 
-    -- REQUIRED
     harpoon:setup()
-
-    -- Basic keymaps
     local keymap = vim.keymap.set
-    -- Menu toggle
+
     keymap('n', '<leader>h', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'Open harpoon window' })
+    end, { desc = 'Open Harpoon window' })
 
-    -- Adding marks
+    for i = 1, 9 do
+      keymap('n', '<leader>' .. i, function()
+        harpoon:list():select(i)
+      end, { desc = 'Go to Harpoon file ' .. i })
+    end
+    keymap('n', '<leader>0', function()
+      harpoon:list():select(10)
+    end, { desc = 'Go to Harpoon file 10' })
+
     keymap('n', '<leader>a', function()
       harpoon:list():append()
-    end, { desc = 'Add file to harpoon' })
+    end, { desc = 'Add file to Harpoon' })
 
-    -- File navigation
-    --keymap("n", "<C-h>", function() harpoon:list():select(1) end,
-    --{ desc = "Navigate to harpoon file 1" })
-    --keymap("n", "<C-j>", function() harpoon:list():select(2) end,
-    --{ desc = "Navigate to harpoon file 2" })
-    --keymap("n", "<C-k>", function() harpoon:list():select(3) end,
-    --{ desc = "Navigate to harpoon file 3" })
-    --keymap("n", "<C-l>", function() harpoon:list():select(4) end,
-    --{ desc = "Navigate to harpoon file 4" })
-
-    -- Toggle previous & next buffers stored within Harpoon list
     keymap('n', '<C-p>', function()
       harpoon:list():prev()
-    end, { desc = 'Navigate to previous harpoon file' })
+    end, { desc = 'Previous Harpoon file' })
     keymap('n', '<C-n>', function()
       harpoon:list():next()
-    end, { desc = 'Navigate to next harpoon file' })
+    end, { desc = 'Next Harpoon file' })
   end,
 }
